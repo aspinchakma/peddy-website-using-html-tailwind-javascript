41c9345a9756ae285b941e0d47736aaa72af2605
Aspin Chakma <aspinchakma2019@gmail.com>
2025-08-10 16:37:22 +0600

    finished nav bar

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,1 +1,11 @@
-export const name = "Aspin Chakma";
+// Get data using api
+async function loadData(api) {
+  try {
+    const res = await fetch(api);
+    const data = await res.json();
+  } catch (error) {}
+}
+
+// Show warning
+
+// Show Spinner
