656b642da279facd912352597cd0b3496852eb46
Aspin Chakma <aspinchakma2019@gmail.com>
2025-08-11 00:38:23 +0600

    categories section functionality done

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,9 @@
+import {
+  selectedCategory,
+  selectedCategoryLoadData,
+} from "../scripts/utilities.js";
 const categoriesApi = `https://openapi.programming-hero.com/api/peddy/categories`;
+
 async function loadCategories() {
   try {
     const res = await fetch(categoriesApi);
@@ -9,11 +14,11 @@ async function loadCategories() {
     showCategories(data.categories);
   } catch (error) {}
 }
+const categoriesContainer = document.getElementById("pet_category_section");
 function showCategories(data) {
-  const categoriesContainer = document.getElementById("pet_category_section");
   data.forEach((item) => {
     const div = document.createElement("div");
-    div.id = `${item.id}`;
+    div.id = `${item.category}`;
     div.classList.add("category");
     // div.classList.add("selectedCategory");
     div.innerHTML = `
@@ -23,4 +28,25 @@ function showCategories(data) {
     categoriesContainer.appendChild(div);
   });
 }
+// Display All Data
+const allDataAPI = `https://openapi.programming-hero.com/api/peddy/pets`;
+selectedCategoryLoadData(allDataAPI, "pet_container");
+
+// Clicked Categories
+categoriesContainer.addEventListener("click", (e) => {
+  const targetId = e.target.closest("div").id;
+  if (targetId === "pet_category_section") {
+    return;
+  }
+  const targetDiv = e.target.closest("div");
+  const children = e.target.closest("div").parentNode.children;
+  console.log(e.target.closest("div").parentNode.children);
+  selectedCategory(children, targetDiv);
+
+  //  categorywise data
+  const url = `https://openapi.programming-hero.com/api/peddy/category/${targetId}`;
+  selectedCategoryLoadData(url, "pet_container");
+  console.log(url);
+});
+
 loadCategories();
---
+++ scripts/utilities.js
@@ -0,0 +1,68 @@
+export function selectedCategory(data, selected) {
+  for (let category of data) {
+    category.classList.remove("selectedCategory");
+  }
+  selected.classList.add("selectedCategory");
+}
+
+export async function selectedCategoryLoadData(url, container) {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    if (data.pets === undefined) {
+      showData(data.data, container);
+      return;
+    }
+
+    showData(data.pets, container);
+  } catch (error) {}
+}
+
+function showData(data, container) {
+  if (data.length !== 0 || data.length === undefined);
+  const petContainer = document.getElementById(container);
+  petContainer.innerHTML = "";
+  data.forEach((pet) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+      <div class="card bg-base-100 w-full shadow-sm pet p-5">
+            <figure>
+              <img
+                src="${pet.image}"
+                alt="Shoes"
+                class="rounded-xl"
+              />
+            </figure>
+            <div>
+              <h2 class="card-title text-2xl my-2">${pet.pet_name}</h2>
+              <p>
+                <i class="fa-solid fa-dna"></i
+                ><span>Breed: ${pet.breed ? pet.breed : "conceal"}</span>
+              </p>
+              <p>
+                <i class="fa-regular fa-calendar"></i> <span>Birth: ${
+                  pet.date_of_birth ? pet.date_of_birth : "Private"
+                }</span>
+              </p>
+              <p>
+                <i class="fa-solid fa-mercury"></i> <span>Gender: ${
+                  pet.gender ? pet.gender : "Surprise"
+                }</span>
+              </p>
+              <p>
+                <i class="fa-solid fa-dollar-sign"></i><span>Price : ${
+                  pet.price
+                }$</span>
+              </p>
+              <div class="card-actions flex justify-between mt-4">
+                <i class="fa-regular fa-thumbs-up like"></i>
+                <button class="adopt">Adopt</button>
+                <button class="details_button">Details</button>
+              </div>
+            </div>
+          </div>
+      
+      `;
+    petContainer.appendChild(div);
+  });
+}
